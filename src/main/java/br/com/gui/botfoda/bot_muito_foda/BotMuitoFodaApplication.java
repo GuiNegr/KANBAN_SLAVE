2e6fc6281e200bccb65cd64952d65675bb85c0cf
guiNegr <guineg.git@outlook.com>
2026-03-10 22:35:20 -0300

    first

---
+++ src/main/java/br/com/gui/botfoda/bot_muito_foda/BotMuitoFodaApplication.java
@@ -0,0 +1,13 @@
+package br.com.gui.botfoda.bot_muito_foda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotMuitoFodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotMuitoFodaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gui/botfoda/bot_muito_foda/BotMuitoFodaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gui.botfoda.bot_muito_foda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotMuitoFodaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
